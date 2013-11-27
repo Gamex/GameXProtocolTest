6933be84e77a77331c9b705d111caa0a302f61d8
559210 <552910@qq.com>
2013-11-27 17:37:34 +0800

    add some test case

--- GameXProtocolTest/CTestLogin.cpp
+++ GameXProtocolTest/CTestLogin.cpp
@@ -16,9 +16,46 @@ bool CTestLogin::onEnter()
 {
     if (!CPomeloTestBase::onEnter()) return false;
     
-    // ---------------
-    addTask(
-            [&](void)
+    // --------------- prepare test
+    addTask([&](void)
+            {
+                setTaskName("prepare test");
+                const char *route = "gate.gateHandler.queryEntry";
+                json_t *msg = json_object();
+                json_object_set(msg, "userName", json_string("autoTest"));
+                json_object_set(msg, "password", json_string("at123"));
+                POMELO->request(route, msg,
+                                [&](Node* node, void* resp)
+                                {
+                                    CCPomeloReponse* ccpomeloresp = (CCPomeloReponse*)resp;
+                                    json_t* code = json_object_get(ccpomeloresp->docs, "code");
+                                    if (200 != json_integer_value(code))
+                                    {
+                                        setTaskState(TASK_FAIL);
+                                        return;
+                                    }
+                                    
+                                    const char* route = "gate.gateHandler.admin_kickAllOnlineUser";
+                                    json_t *msg = json_object();
+                                    POMELO->request(route, msg,
+                                                    [&](Node* node, void* resp)
+                                                    {
+                                                        CCPomeloReponse* ccpomeloresp = (CCPomeloReponse*)resp;
+//                                                        printf(json_dumps(ccpomeloresp->docs, JSON_COMPACT));
+                                                        json_t* code = json_object_get(ccpomeloresp->docs, "code");
+                                                        if (200 != json_integer_value(code))
+                                                        {
+                                                            setTaskState(TASK_FAIL);
+                                                            return;
+                                                        }
+                                                        
+                                                        setTaskState(TASK_OK);
+                                                    });
+                                });
+            });
+    
+    // --------------- login ok
+    addTask([&](void)
             {
                 setTaskName("login ok");
                 const char *route = "gate.gateHandler.queryEntry";
@@ -40,9 +77,29 @@ bool CTestLogin::onEnter()
                                 });
             });
 
-    // ---------------
-    addTask(
-            [&](void)
+    // --------------- use kick without authority
+    addTask([&](void)
+            {
+                setTaskName("use kick without authority");
+                const char* route = "gate.gateHandler.admin_kickAllOnlineUser";
+                json_t *msg = json_object();
+                POMELO->request(route, msg,
+                                [&](Node* node, void* resp)
+                                {
+                                    CCPomeloReponse* ccpomeloresp = (CCPomeloReponse*)resp;
+                                    json_t* code = json_object_get(ccpomeloresp->docs, "code");
+                                    if (200 == json_integer_value(code))
+                                    {
+                                        setTaskState(TASK_FAIL);
+                                        return;
+                                    }
+                                    
+                                    setTaskState(TASK_OK);
+                                });
+            });
+    
+    // --------------- login error username
+    addTask([&](void)
             {
                 setTaskName("login error username");
                 const char *route = "gate.gateHandler.queryEntry";
@@ -64,9 +121,8 @@ bool CTestLogin::onEnter()
                                 });
             });
     
-    // ---------------
-    addTask(
-            [&](void)
+    // --------------- login error password
+    addTask([&](void)
             {
                 setTaskName("login error password");
                 const char *route = "gate.gateHandler.queryEntry";
@@ -87,6 +143,8 @@ bool CTestLogin::onEnter()
                                     setTaskState(TASK_OK);
                                 });
             });
+    
+    
 	return true;
 }
 
